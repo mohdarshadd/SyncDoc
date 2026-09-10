b456c8c922e2bc0fed1690d7964617117059aa7a
Mohd Arshad <mohdarshad1034@gmail.com>
2026-09-10 09:00:00 +0530

    Add useMediaQuery hook with test matchMedia polyfill

--- frontend/test/setup.js
+++ frontend/test/setup.js
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom'
+
+if (typeof window !== 'undefined' && typeof window.matchMedia !== 'function') {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      addListener: () => {},
+      removeListener: () => {},
+      dispatchEvent: () => false,
+    }),
+  })
+}
